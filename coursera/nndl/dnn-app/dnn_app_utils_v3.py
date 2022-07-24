6d90550f47865bf49a9ea271f6b6ea3a88ca0e08
volodymyrd <volodymyrdotsenko@gmail.com>
2022-07-25 00:16:58 +0200

    update load data util method

--- coursera/nndl/dnn-app/dnn_app_utils_v3.py
+++ coursera/nndl/dnn-app/dnn_app_utils_v3.py
@@ -88,11 +88,11 @@ def sigmoid_backward(dA, cache):
 def load_data():
     print(f'Current dir {os.path.dirname(os.path.realpath(__file__))}')
     print(f'List dir {os.listdir()}')
-    train_dataset = h5py.File('../datasets/train_catvnoncat.h5', "r")
+    train_dataset = h5py.File('nn/coursera/nndl/dnn-app/datasets/train_catvnoncat.h5', "r")
     train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
     train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
 
-    test_dataset = h5py.File('../datasets/test_catvnoncat.h5', "r")
+    test_dataset = h5py.File('nn/coursera/nndl/dnn-app/datasets/test_catvnoncat.h5', "r")
     test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
     test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
 
